10d4a0f8b1272b47ac67471ec1cb1f597a0729c0
whitefusion <whitefusion@163.com>
2018-03-31 21:12:47 +0800

    convert json to csv

--- grade_table.py
+++ grade_table.py
@@ -2,10 +2,10 @@ import csv
 import json
 grade_list = []
 with open("list-of-nea-licensed-eating-establishments.csv","r") as f:
-    raw_data = csv.reader(f)
-    data = list(raw_data)
+    f_csv = csv.reader(f)
+    data = next(f_csv)
 
-    for row in data[1:]:
+    for row in f_csv:
         temp = {}
         temp["planning area"] = row[6]
         temp["grade"] = row[3]
@@ -24,6 +24,20 @@ for a in area_set:
     grade_table[a]['sum'] = grade_table[a]['A']+grade_table[a]['B']+grade_table[a]['C']
     grade_table[a]['A_percent']=grade_table[a]['A']/float(grade_table[a]['sum'])
     grade_table[a]['B_percent']=grade_table[a]['B']/float(grade_table[a]['sum'])
-with open("grade_table.json",'wb') as f:
-    json.dump(grade_table, f)
+
+headers = ['planning area','A','B','C','A_percent','B_percent','sum']
+rows= [(a,
+           grade_table[a]['A'],
+           grade_table[a]['B'],
+           grade_table[a]['C'],
+           grade_table[a]['A_percent'],
+           grade_table[a]['B_percent'],
+           grade_table[a]['sum']) for a in list(area_set)]
+
+print(rows)
+
+with open("grade_table.csv",'w') as f:
+    f_csv = csv.writer(f)
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
 
